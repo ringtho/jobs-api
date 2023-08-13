d4c6cdb773a7c73988ac249f64e754b0c0dfd7b3
Smith Ringtho <sringtho@gmail.com>
2023-08-13 23:33:43 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+
+
+const PORT = process.env.PORT || 5050
+app.listen(PORT, console.log(`Server listening on port ${PORT}`))

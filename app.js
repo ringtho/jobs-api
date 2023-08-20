2a07f6180e106bf9f2238e177bebd78fde357a08
Smith Ringtho <sringtho@gmail.com>
2023-08-20 16:36:32 +0300

    Added swagger documentation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// swagger Documentation
+const YAML = require('yamljs')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 const express = require('express')
 const app = express()
 
@@ -34,8 +39,10 @@ app.use(cors())
 app.use(xss())
 
 app.get('/', (req, res) => {
-  res.send('JOBS API')
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 app.use('/api/v1/auth', authRouter)
 
